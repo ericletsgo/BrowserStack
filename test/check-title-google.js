22b8d4f03e0e6fbe6fbe12c3ede5f1615ba94e19
Eric Wang <ericletsgo@gmail.com>
2020-05-10 19:42:38 -0700

    took out comments on test files

--- test/check-title-google.js
+++ test/check-title-google.js
@@ -1,17 +1,4 @@
 module.exports = function helloBrowserStack(browser, next) {
-    /*
-	next must be called by this method somewhere. 
-	in case test fails next can be passed some error message.
-	
-	eg:
-	
-	if (title === 'Google') {
-      	     next(null);
-        } else {
-            next(new Error('UH OH Title is not ok! Are you on google.com?'));
-        }
-    */
-
     browser.title(function(err, title) {
         if (title === 'Google') {
             console.log("PASS: Title for the page: " + title);
@@ -20,14 +7,5 @@ module.exports = function helloBrowserStack(browser, next) {
             console.log('FAIL: ' + err);
             next(err);
         }
-        // browser.elementByName('q', function(err, el) {
-        //     el.sendKeys("BrowserStack", function() {
-        //         browser.elementByName("btnG", function(err,el) {
-        //             el.click(function() {
-        //                 next(null);
-        //             })
-        //         })
-        //     })
-        // })
     })
 }
