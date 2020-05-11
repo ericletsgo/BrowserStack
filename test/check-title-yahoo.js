68a96624502aeeb545f8da12f7b15042f403cdc1
Eric Wang <ericletsgo@gmail.com>
2020-05-11 07:33:04 -0700

    made indent uniform

--- test/check-title-yahoo.js
+++ test/check-title-yahoo.js
@@ -1,11 +1,11 @@
 module.exports = function helloBrowserStack(browser, next) {
-  browser.title(function(err, title) {
-    if (title === 'Yahoo') {
-      console.log("PASS: Title for the page: " + title);
-      next(null);
-    } else {
-      console.log('FAIL: ' + err);
-      next(err);
-    }
-  });
+    browser.title(function(err, title) {
+        if (title === 'Yahoo') {
+            console.log("PASS: Title for the page: " + title);
+            next(null);
+        } else {
+            console.log('FAIL: ' + err);
+            next(err);
+        }
+    });
 }
--- test/check-url.js
+++ test/check-url.js
@@ -1,12 +1,12 @@
 module.exports = function helloBrowserStack(browser, next) {
-  let expectedURL = 'http://www.google.com/';
-  browser.url(function(err, url) {
-    if (url === expectedURL) {
-      console.log ('PASS: Actual and expected are the same');
-      next(null);
-    } else {
-      console.log ('FAIL: actual url:' + url + ' ERROR: ' + err);
-      next(err);
-    }
-  });
+    let expectedURL = 'http://www.google.com/';
+    browser.url(function(err, url) {
+        if (url === expectedURL) {
+            console.log ('PASS: Actual and expected are the same');
+            next(null);
+        } else {
+            console.log ('FAIL: actual url:' + url + ' ERROR: ' + err);
+            next(err);
+        }
+    });
 }
